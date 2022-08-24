1a97fe3f303952b3cf089d79f8e6545a6c4d0356
tuyenhv@tienngay.vn <tuyenhv@tienngay.vn>
2022-08-24 13:43:19 +0700

    write scss and pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Todos from './Todos';
+import Content from './Content';
+import { useState, useEffect, useContext, createContext, useRef, useReducer, useCallback, useMemo} from 'react';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Updated2 <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  const [products, setProducts] = useState([]);
+  const hRedFocus = useRef();
+  const handleSubmit = () => {
+    setProducts([...products, {
+      name,
+      price: +price
+    }]);
+      setName('')
+      setPrice('')
+      hRedFocus.current.focus();
+  }
+
+  //sử dụng useMemo để không lặp lại logic tính toán tổng tiền result
+  const total = useMemo(() => {
+    const result = products.reduce((result, prod) => {
+       return result + prod.price
+      },0)
+      return result;
+      // khi Array products thay đổi sẽ tính toán lại tổng giá price
+  },[products])
+  
+  
+    return (
+      <div style={{padding: 50}}>
+        <label>Enter your product </label>
+        <input
+          ref={hRedFocus}
+          value={name}
+          name='name'
+          onChange={(e) => setName(e.target.value)}
+        />
+        <br></br>
+        <label>Enter your price </label>
+        <input 
+        value={price}
+        name='price'
+        onChange={(e) => setPrice(e.target.value)}
+        />
+        <br></br>
+        <button onClick={handleSubmit} >Add </button>
+        <p>Total: {total}</p>
+        <ul>
+          {products.map((product, index) => (
+            <li key={index}>
+              {product.name} - {product.price}
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
 }
+
 
 export default App;
